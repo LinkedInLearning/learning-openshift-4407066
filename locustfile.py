cbdcf186acbd98d3195302337adfa824dae4627b
Adrian Kosmaczewski <akosma@protonmail.com>
2023-06-23 15:19:22 +0200

    Added demo for serverless video

---
+++ locustfile.py
@@ -0,0 +1,7 @@
+from locust import FastHttpUser, task
+
+class OpenShiftUser(FastHttpUser):
+    @task
+    def knative_sleeper(self):
+        self.client.get("/sleep")
+
---
+++ steps.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+########################
+# Include the magic
+########################
+. ~/demo-magic.sh
+
+# Variables
+PROJECT=serverless-demo
+
+# Hide the evidence
+oc logout
+clear
+
+# Actual demo
+pe "oc login -u developer -p developer https://api.crc.testing:6443"
+pe "oc new-project $PROJECT"
+wait
+clear
+
+pei "bat service.yml"
+pe "oc apply -f service.yml"
+wait
+clear
+
+pei "bat locustfile.py"
+pe "locust -u 500 -r 10 -t 30s -H https://fortune-$PROJECT.apps-crc.testing --autostart --autoquit 1 --headless"
+wait
+clear
+
+cmd
+
