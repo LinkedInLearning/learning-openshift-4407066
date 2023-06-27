f77cc21dcbd538115c540d3d90027de7f52bd10f
Adrian Kosmaczewski <akosma@protonmail.com>
2023-06-27 16:54:20 +0200

    Better demo

--- demo.sh
+++ demo.sh
@@ -8,5 +8,7 @@
 DEMO_PROMPT="${GREEN}➜  ${CYAN}04_02 ${COLOR_RESET}"
 
 clear
+pe "oc apply -f cluster-monitoring-config.yaml"
+pe "oc process -f 00-postgresql-template.yaml | oc apply -f -"
 pe "oc apply -f 01-app.yaml"
 wait
