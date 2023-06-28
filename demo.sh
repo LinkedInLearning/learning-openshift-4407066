042e205dd047c8c1015471c550af58c1992f89c6
Adrian Kosmaczewski <akosma@protonmail.com>
2023-06-28 19:40:59 +0200

    Complete example

--- demo.sh
+++ demo.sh
@@ -13,4 +13,8 @@ pe "oc apply -f 02-taskrun.yaml"
 pe "oc apply -f 03-goodbye.yaml"
 pe "oc apply -f 04-pipeline.yaml"
 pe "oc apply -f 05-pipelinerun.yaml"
+pe "oc apply -f 06-clone-kaniko-pipeline.yaml"
+pe "oc whoami -t | podman login -u kubeadmin --password-stdin default-route-openshift-image-registry.apps-crc.testing"
+pe "podman pull default-route-openshift-image-registry.apps-crc.testing/my-first-pipeline/my-image:latest"
+pe "podman images"
 wait
