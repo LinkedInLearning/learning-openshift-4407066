bbaa85e5a07d0552e161e5b830835d34b5254fae
Adrian Kosmaczewski <akosma@protonmail.com>
2023-06-30 15:02:02 +0200

    Updated demo

--- demo.sh
+++ demo.sh
@@ -5,10 +5,26 @@
 . ~/demo-magic.sh
 
 # Variables
-DEMO_PROMPT="${GREEN}➜  ${CYAN}04_03 ${COLOR_RESET}"
+DEMO_PROMPT="${GREEN}➜  ${CYAN}04_05 ${COLOR_RESET}"
 
 clear
+p "crc delete"
+p "crc config set enable-cluster-monitoring true"
+p "crc start --memory 20480 --cpus 6 --disk-size 300 --nameserver 1.1.1.1 --pull-secret-file ./pull-secret"
+wait
+
+clear
+pe "bat 01-cluster-monitoring-config.yaml"
 pe "oc apply -f 01-cluster-monitoring-config.yaml"
+wait
+
+clear
+pe "bat 02-app.yaml"
 pe "oc apply -f 02-app.yaml"
+wait
+
+clear
+pe "bat 03-service-monitor.yaml"
 pe "oc apply -f 03-service-monitor.yaml"
 wait
+
